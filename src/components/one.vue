b9eeb12a6dcdf37c7aec2536c304ac9a56315ac9
yosiaLukumai <yosialukumai@gmail.com>
2022-12-17 23:32:18 +0300

    files components

---
+++ src/components/one.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="row " style="padding:none !important; margin:none !important;">
+    <div class="col-8  blacked borderleft">
+      <div class="q-pt-xl text-primary text-h4 q-pl-xl text-weight-bolder ">Architectural <span
+          class="text-italic">Studio One</span> </div>
+      <div class="q-pt-xl">
+      </div>
+      <div class="q-pt-md">
+      </div>
+      <div class="blured" v-if="currentSche">
+        <div class="q-pa-lg q-py-md text-white " style="width:100%">
+          <div class="text-h4 q-pl-lg text-weight-bolder">{{ currentSche?.subjectName }}
+            ({{ currentSche?.codename }})</div>
+          <div class="text-h4  q-pl-lg  q-pt-sm text-weight-bolder">{{ createTime(currentSche?.startTime) }} -
+            {{ createTime(currentSche.endTime) }}</div>
+          <div class="q-pt-sm  q-pl-lg  text-italic text-bold text-h6">{{ currentSche?.teacher }} <span
+              class="text-primary">--</span> {{ currentSche?.classLevel }}</div>
+        </div>
+      </div>
+      <div v-else class="blured">
+        <div class="q-py-xl q-pl-xl text-h4 text-weight-bolder text-white">No. Schedule</div>
+      </div>
+
+    </div>
+    <div class="col-4 darked" style="height: 99.8vh;">
+      <div class="q-pa-md">
+        <div class="q-pt-md text-h1 text-bold text-white text-center">
+          {{ time }}
+        </div>
+        <div class="text-center text-bold text-h6 text-white">{{ day }}, {{ month.slice(0, 3) }} {{ date }}</div>
+      </div>
+      <div class="q-pt-md">
+        <div class="text-h6 q-pl-md text-bold text-white underline">Coming up</div>
+        <upcomingVue :upComing="upComing" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import upcomingVue from "./upcoming.vue"
+import { filterLater, checkInBetween, createTime } from "src/utils/date"
+import { scheduleStore } from "src/stores/schedule"
+import { ref, onMounted } from "vue"
+export default {
+  components: { upcomingVue },
+  props: {
+    time: {
+      type: String,
+      default: "12:34"
+    },
+    day: {
+      type: String,
+      default: "Thursday"
+    },
+    month: {
+      type: String,
+      default: "December"
+    },
+    date: {
+      type: String,
+      default: "15"
+    },
+
+  },
+  setup(props) {
+    const str = scheduleStore()
+    const data = ref(str.one)
+    const upComing = ref(null)
+    const currentSche= ref(null)
+    onMounted(() => {
+      upComing.value = filterLater(data.value).slice(0, 3)
+      currentSche.value = checkInBetween(data.value)
+    })
+    return { upComing, currentSche, createTime }
+
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.borderleft {
+  border-left: 14px solid $secondary;
+}
+</style>
---
+++ src/components/upcoming.vue
@@ -0,0 +1,35 @@
+<template>
+    <div v-for="(item, index) in upComing" :key="index">
+        <div class="q-pl-md">
+            <q-separator color="primary" size="0.1" class=""></q-separator>
+        </div>
+        <div class="q-py-md row q-pl-md text-white">
+            <div class="col-8 text-body1 text-weight-medium">
+                {{ item.subjectName }}
+            </div>
+            <div class="col-4 text-body1 text-weight-medium">
+                {{ item.startTime }} -{{ item.endTime }}
+            </div>
+        </div>
+        <div class="q-pl-md">
+            <q-separator color="primary" size="0.1" class=""></q-separator>
+        </div>
+        <div class="q-pl-md">
+            <q-separator color="primary" size="0.1" class=""></q-separator>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        upComing: {
+            type: Object
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
